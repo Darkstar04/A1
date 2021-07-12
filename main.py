f757e23021a3678619ca9aa061d92acf0cc7fcee
Darkstar04 <71513700+Darkstar04@users.noreply.github.com>
2021-07-12 23:04:30 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -102,16 +102,16 @@ def X_3(X1, X2):
     A1 = numpy.array(X1)
     X1 = numpy.array(X1)
     X2 = numpy.array(X2)
-    for part in Annotations(X2):
-        x = math.ceil(part.x)
-        y = math.ceil(part.y)
-        h = math.ceil(part.h / 2)
-        w = math.ceil(part.w / 2)
-        if part == 'tit': cv2.ellipse(A1, (x, y), (h, w), 0, 0, 360, (0, 205, 0), -1)
-        if part == 'aur': cv2.ellipse(A1, (x, y), (h, w), 0, 0, 360, (255, 0, 0), -1)
-        if part == 'nip': cv2.ellipse(A1, (x, y), (h, w), 0, 0, 360, (255, 255, 255), -1)
-        if part == 'bel': cv2.ellipse(A1, (x, y), (h, w), 0, 0, 360, (255, 0, 255), -1)
-        if part == 'vag': cv2.ellipse(A1, (x, y), (h, w), 0, 0, 360, (0, 0, 255), -1)
+    for obj in Part(X2, part):
+        x = math.ceil(obj.x)
+        y = math.ceil(obj.y)
+        h = math.ceil(obj.h / 2)
+        w = math.ceil(obj.w / 2)
+        if obj.name == 'tit': cv2.ellipse(A1, (x, y), (h, w), 0, 0, 360, (0, 205, 0), -1)
+        if obj.name == 'aur': cv2.ellipse(A1, (x, y), (h, w), 0, 0, 360, (255, 0, 0), -1)
+        if obj.name == 'nip': cv2.ellipse(A1, (x, y), (h, w), 0, 0, 360, (255, 255, 255), -1)
+        if obj.name == 'bel': cv2.ellipse(A1, (x, y), (h, w), 0, 0, 360, (255, 0, 255), -1)
+        if obj.name == 'vag': cv2.ellipse(A1, (x, y), (h, w), 0, 0, 360, (0, 0, 255), -1)
     mask = cv2.inRange(X1, (0, 255, 0), (0, 255, 0))
     mask_invert = numpy.invert(mask)
     X_3 = cv2.bitwise_and(X1, X1, mask=mask_invert) + cv2.bitwise_and(A1, A1, mask=mask)
@@ -125,14 +125,6 @@ class BodyPart:
         self.y = y
         self.h = h
         self.w = w
-
-def Annotations(X2):
-    tit = Part(X2, 'tit')
-    aur = Part(X2, 'aur')
-    bel = Part(X2, 'bel')
-    vag = Part(X2, 'vag')
-    nip = Nip(aur)
-    return tit + aur + nip + bel + vag
 
 def Part(X2, part):
     bodypart = []
