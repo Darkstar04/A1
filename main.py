97ec9c19729613fbed3d0fee8191fb610073f3f7
Darkstar04 <71513700+Darkstar04@users.noreply.github.com>
2021-07-13 17:39:26 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -154,7 +154,7 @@ def Part(X2, part):
 def Nip(aur):
     nip = []
     for aur in aur:
-        nip_dim = int(5 + aur.w*random.uniform(0.5, 0.9))
+        nip_dim = int(5 + aur.w*random.uniform(0.5, 0.5))
         nip.append(BodyPart('nip', aur.x, aur.y, nip_dim, nip_dim))
     return nip
 
